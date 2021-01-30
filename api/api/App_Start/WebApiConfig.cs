41eeb224234f6db3903838657957757afead3f8c
Hamdi Damar <hmdmr1@hotmail.com>
2021-01-30 12:49:58 +0300

    api-v1

---
+++ api/api/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API yapılandırması ve hizmetleri
+
+            //Enable config
+            config.EnableCors();
+
+            // Web API yolları
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Remove xlm format
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
